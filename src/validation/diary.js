4f3402b2c6ba2c617e68471bc5732eba83cf7ee2
Burak Ezer <burakezer@outlook.com>
2025-12-26 17:23:42 +0300

    validation fix

--- src/validation/diary.js
+++ src/validation/diary.js
@@ -1,15 +1,15 @@
 import Joi from 'joi';
 
 export const createDiarySchema = Joi.object({
-  weight: Joi.number().min(0).max(400),
-  date: Joi.string(),
-  productId: Joi.string(),
+  weight: Joi.number().min(0).max(400).required(),
+  date: Joi.string().required(),
+  productId: Joi.string().required(),
 });
 
 export const delDiarySchema = Joi.object({
-  dailyId: Joi.string(),
+  dailyId: Joi.string().required(),
 });
 
 export const getDiarySchema = Joi.object({
-  date: Joi.string(),
+  date: Joi.string().required(),
 });
