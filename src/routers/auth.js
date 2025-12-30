66142771a305ae97bf52a83410c07e55a58e6dc5
Ozan Can CUYAR <ozncncyr@outlook.com>
2025-12-30 20:48:12 +0300

    remove authentication middleware from logout route

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -29,7 +29,7 @@ router.post(
   ctrlWrapper(loginUserController),
 );
 
-router.post('/logout', authenticate, ctrlWrapper(logoutUserController));
+router.post('/logout', ctrlWrapper(logoutUserController));
 
 router.get('/refresh', authenticate, ctrlWrapper(refreshUserSessionController));
 
